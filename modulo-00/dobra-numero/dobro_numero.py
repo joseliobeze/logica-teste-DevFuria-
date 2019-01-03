6413c158ca54f80f787208fdaaaf68146309db8f
Joselio bezerra <joseliobeze@gmail.com>
2019-01-03 16:46:48 -0200

    Arquivos recolocados e organizados

--- modulo-00/dobra-numero/dobro_numero.py
+++
@@ -1,8 +0,0 @@
-def dobro(n):
-	return  n * 2
-	
-assert 8 == dobro(4), 'O dobro de 4 deve ser 8'
-assert 20 == dobro(10), 'O dobro de 10 deve ser 20'
-assert 4 == dobro(2), 'O dobro de 2 deve ser 4'
-assert 30 == dobro(15), 'O dobro de 15 deve ser 30'
-assert 50 == dobro(25), 'O dobro de 25 deve ser 50'
